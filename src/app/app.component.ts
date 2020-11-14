8eec9e0b430fb7fc9ec571c71f2ccd6ce81a5944
samet <samet.development@gmail.com>
2020-11-14 23:05:53 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {
+  }
   title = 'game-room';
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000').subscribe(res => {
+      console.log(res)
+    })
+  }
 }
