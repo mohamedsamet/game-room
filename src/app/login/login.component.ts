5578197cf83d1df28e1e04dbf6093a58eba6d49c
samsif <mohamed.samet@sifast.com>
2020-11-17 11:30:29 +0100

    add bcrypt front

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { UserInterface } from '../interfaces/user-interface/user.interface';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserModel } from '../models/user/user.model';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   public loginFormGroup: FormGroup;
+  public loginError = false;
   constructor(@Inject('UserInterface') private  userInt: UserInterface, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -18,8 +20,25 @@ export class LoginComponent implements OnInit {
   }
 
   validatePseudo(): void {
-    this.userInt.addUserByPseudo(this.loginFormGroup.get('pseudo')?.value).subscribe(res => {
-      console.log(res);
+    this.userInt.addUserByPseudo(this.loginFormGroup.get('pseudo')?.value).subscribe((user: UserModel)  => {
+      this.loginToHome(user);
+    }, error => {
+      this.loginError = true;
     });
+  }
+
+  loginToHome(user: UserModel): void {
+    if (user.hash) {
+      this.saveHashToStorage(user.hash);
+      this.redirectToHome();
+    } else {
+      this.loginError = true;
+    }
+  }
+
+  redirectToHome(): void {}
+
+  saveHashToStorage(hash: string): void {
+    localStorage.setItem('hash', hash);
   }
 }
