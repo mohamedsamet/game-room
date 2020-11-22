3e443d405a68b9bb91f074b54720842465df1001
samet <samet.development@gmail.com>
2020-11-22 19:02:31 +0100

    fix on init bug

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent {
   @Input() pagesNumber: number;
   @Output() selectedPageEvent = new EventEmitter<number>();
   public selectedPage: number;
