626f42c724693418c0efe7f1bbdc61e2c3431bf0
samsif <samet.development@gmail.com>
2021-01-04 16:10:35 +0100

    config env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000',
-  socketUrl: 'http://localhost:3002'
+  baseUrl: 'http://localhost:3005',
+  socketUrl: 'http://localhost:3007'
 };
