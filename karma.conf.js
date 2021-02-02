4e667c79692ece0ceefcc6d6efde7d818687846b
samsif <samet.development@gmail.com>
2021-02-02 13:49:25 +0100

    update watch config

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,6 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
