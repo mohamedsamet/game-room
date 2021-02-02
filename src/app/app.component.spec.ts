e196d66ab4da4e087e3d9852e2d5a57e1888c795
samsif <samet.development@gmail.com>
2021-02-02 13:13:24 +0100

    app component unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,123 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LoggedUserInterface } from './interfaces/user/logged-user.interface';
+import { LOCAL_STORAGE_ID } from './constants/rooms.constant';
+import { RedirectionInterface } from './interfaces/utilities/redirection/redirection.interface';
+import { Observable, of, throwError } from 'rxjs';
+import { Component } from '@angular/core';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let redirectInt: any;
+  let loggedUserInt: any;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      imports: [RouterTestingModule],
+      providers: [
+        {provide: 'LoggedUserInterface', useClass: LoggedUserInterfaceMock},
+        {provide: 'RedirectionInterface', useClass: RedirectionInterfaceMock}],
       declarations: [
-        AppComponent
+        AppComponent,
+        AlertComponentMock
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    redirectInt = TestBed.get('RedirectionInterface')
+    loggedUserInt = TestBed.get('LoggedUserInterface')
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'game-room'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('game-room');
-  });
+  describe('manageAutoLogin method', () => {
+    afterEach(() => {
+      localStorage.removeItem(LOCAL_STORAGE_ID);
+    })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('game-room app is running!');
-  });
+    it('should call manageAutoLogin on init', () => {
+      spyOn(app, 'manageAutoLogin');
+      app.ngOnInit();
+      expect(app.manageAutoLogin).toHaveBeenCalled();
+    })
+
+    it('should call getLoggedUser if user credential exists', () => {
+      spyOn(app, 'getLoggedUser');
+      localStorage.setItem(LOCAL_STORAGE_ID, 'test user id');
+      app.manageAutoLogin();
+      expect(app.getLoggedUser).toHaveBeenCalled();
+    })
+
+    it('should not call getLoggedUser if user credential does not exists', () => {
+      spyOn(app, 'getLoggedUser');
+      app.manageAutoLogin();
+      expect(app.getLoggedUser).not.toHaveBeenCalled();
+    })
+
+    it('should redirect to / if user credential does not exists', () => {
+      spyOn(redirectInt, 'redirectTo')
+      app.manageAutoLogin();
+      expect(redirectInt.redirectTo).toHaveBeenCalledWith('/');
+    })
+
+    it('should not redirect to / if user credential exist', () => {
+      spyOn(redirectInt, 'redirectTo')
+      localStorage.setItem(LOCAL_STORAGE_ID, 'test user id');
+      app.manageAutoLogin();
+      expect(redirectInt.redirectTo).not.toHaveBeenCalledWith('/');
+    })
+  })
+
+  describe('getLoggedUser method', () => {
+    it('should call getLoggedUser from service', () => {
+      spyOn(loggedUserInt, 'getLoggedUser').and.callThrough()
+      app.getLoggedUser()
+      expect(loggedUserInt.getLoggedUser).toHaveBeenCalled()
+    })
+
+    it('should call setUserName from logged user int', () => {
+      spyOn(loggedUserInt, 'setUserName')
+      app.getLoggedUser()
+      expect(loggedUserInt.setUserName).toHaveBeenCalledWith('samet')
+    })
+
+    it('should redirect to rooms with corresponding pseudo', () => {
+      spyOn(redirectInt, 'redirectTo')
+      loggedUserInt.setError();
+      app.getLoggedUser();
+      expect(redirectInt.redirectTo).toHaveBeenCalledWith('/')
+    })
+  })
+
 });
+
+class RedirectionInterfaceMock {
+  redirectTo(path: string): void {
+  }
+}
+
+class LoggedUserInterfaceMock {
+  isError = false;
+  getLoggedUser(): Observable<any> {
+    if (this.isError) {
+      return throwError('error');
+    }
+    return of({pseudo: 'samet'});
+  }
+
+  setError() {
+    this.isError = true;
+  }
+
+  setUserName(userName: string): void {
+  }
+}
+
+@Component({
+  selector: 'app-alert',
+  template: ''
+})
+class AlertComponentMock {}
