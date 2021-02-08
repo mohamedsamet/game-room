aa10e199c1019f551bcdd5ac546d002573d4fab7
samsif <samet.development@gmail.com>
2021-02-08 08:58:55 +0100

    add spec lint verifier

---
+++ spec.lint.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# use nullglob in case there are no matching files
+
+# create an array with all the filer/dir inside ~/myDir
+arr=(`find ./src/app -name "*.component.ts" -o -name "*.service.ts" `)
+# iterate through array using a counter
+for ((i=0; i<${#arr[@]}; i++)); do
+    #do something to each element of array
+    if ls `dirname ${arr[$i]}`/**.spec.ts; then
+      echo "file exist"
+    else
+      echo "ERROR cannot find spec files" `dirname ${arr[$i]}`/**.spec.ts
+      exit 1
+    fi
+done
